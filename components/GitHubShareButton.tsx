4de3f74ed9f6bffec72b4bcf1ba28da17cac76a3
yuguxx <146349981+yuguxx@users.noreply.github.com>
2024-08-23 14:27:07 +0800

    Update GitHubShareButton.tsx

--- components/GitHubShareButton.tsx
+++ components/GitHubShareButton.tsx
@@ -5,15 +5,15 @@ import styles from './styles.module.css'
 export const GitHubShareButton: React.FC = () => {
   return (
     <a
-      href='https://github.com/transitive-bullshit/nextjs-notion-starter-kit'
+      href='https://husky-blog.vercel.app'
       target='_blank'
       rel='noopener noreferrer'
       className={styles.githubCorner}
       aria-label='View source on GitHub'
     >
       <svg
-        width='80'
-        height='80'
+        width='0'
+        height='0'
         viewBox='0 0 250 250'
         style={{
           fill: '#70B7FD',
